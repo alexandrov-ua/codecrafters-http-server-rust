e51272cfcebb2f7eed25330bc5817ad5b724df4f
Anders <alexandrov.ua@gmail.com>
2026-01-26 23:19:11 +0200

    move to laptop [skip ci]

--- src/http_server.rs
+++ src/http_server.rs
@@ -7,6 +7,33 @@ use crate::middlewares::routing_middleware::RoutingMiddleware;
 use std::io::{BufReader, Write};
 use std::net::TcpListener;
 
+struct MiddlewareChain<'a> {
+    iter: std::cell::RefCell<Box<dyn std::iter::Iterator<Item = Box<dyn HttpMiddleware + 'a>> + 'a>>,
+}
+
+
+
+impl<'a> MiddlewareChain<'a> {
+    fn new(iter: Box<dyn std::iter::Iterator<Item = Box<dyn HttpMiddleware + 'a>> + 'a>) -> Self {
+        MiddlewareChain {
+            iter: std::cell::RefCell::new(iter),
+        }
+    }
+
+    fn call(&self, request: &mut HttpRequest) -> HttpResponse {
+        let current_opt = {
+            let mut borrow_itter = self.iter.borrow_mut();
+            borrow_itter.next()
+        };
+
+        if let Some(current) = current_opt {
+            current.handle(request, &|req: &mut HttpRequest| self.call(req))
+        } else {
+            HttpResponse::new(crate::http_response::HttpStatusCode::NotFound)
+        }
+    }
+}
+
 pub struct HttpServer {
     routing: Option<RoutingMiddleware>,
     middlewares: Vec<Box<dyn HttpMiddleware>>,
@@ -20,25 +47,25 @@ impl HttpServer {
         }
     }
 
-    fn middleware_chain<'a>(
-        &self,
-        mut iter: std::slice::Iter<'a, Box<dyn HttpMiddleware>>,
-        request: &mut HttpRequest,
-    ) -> HttpResponse {
-        if let Some(mw) = iter.next() {
-            let next_fn: Box<dyn Fn(&mut HttpRequest) -> HttpResponse> = Box::new(|req: &mut HttpRequest| {
-                self.middleware_chain(iter.clone(), req)
-            });
-            mw.handle(request, next_fn.as_ref())
-        } else {
-            HttpResponse::new(crate::http_response::HttpStatusCode::NotFound)
-        }
-    }
+    // fn middleware_chain<'a>(
+    //     &self,
+    //     mut iter: std::slice::Iter<'a, Box<dyn HttpMiddleware>>,
+    //     request: &mut HttpRequest,
+    // ) -> HttpResponse {
+    //     if let Some(mw) = iter.next() {
+    //         let next_fn: Box<dyn Fn(&mut HttpRequest) -> HttpResponse> = Box::new(|req: &mut HttpRequest| {
+    //             self.middleware_chain(iter.clone(), req)
+    //         });
+    //         mw.handle(request, next_fn.as_ref())
+    //     } else {
+    //         HttpResponse::new(crate::http_response::HttpStatusCode::NotFound)
+    //     }
+    // }
 
     pub fn run(&mut self, addr: &str) {
         self.middlewares.push(Box::new(LoggingMiddleware::new()));
-        self.middlewares.push(Box::new(self.routing.take().unwrap()));
-
+        self.middlewares
+            .push(Box::new(self.routing.take().unwrap()));
 
         let listener = TcpListener::bind(addr).unwrap();
 
@@ -50,7 +77,7 @@ impl HttpServer {
                         Ok(mut request) => {
                             let iter = self.middlewares.iter();
 
-                            let response = self.middleware_chain(iter, &mut request);
+                            let response = MiddlewareChain::new(Box::new(iter as dyn std::iter::Iterator<Item = Box<dyn HttpMiddleware>>)).call(&mut request);
 
                             _stream
                                 .write_all(Vec::<u8>::from(response).as_slice())
